e4a7d0522b3d0d8de965f7967c8174eae26f5430
verkhohliad <ihor.verkhohliad@gmail.com>
2025-02-07 13:12:06 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "@/pages/home";
+import DetailsPage from "@/pages/details";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<HomePage />} path="/" />
+      <Route element={<DetailsPage />} path="/inscription/:id" />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const HomePage: FC = () => {
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <h1 className="text-2xl font-bold mb-6">Ordinal Inscription Lookup</h1>
+      {/* Search input and results will be added here */}
+    </main>
+  );
+};
+
+export default HomePage;
